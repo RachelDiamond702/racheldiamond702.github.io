22e9583d6a4ae0e997bfde06c85a05289f824214
RachelDiamond702 <radi3284@colorado.edu>
2024-11-05 10:36:44 -0700

    ica11

---
+++ js/ica11.js
@@ -0,0 +1,8 @@
+function tellFourtune(w,x,y,z){
+    const fourtune = 'You will be a ${z} in ${y}, and married to ${x} with ${w} kids.'
+    console.log(fourtune);
+    return fourtune;
+}
+
+tellFourtune(2,'Sally','Colorado', 'Accountant');
+
