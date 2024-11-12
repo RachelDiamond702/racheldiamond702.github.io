01cc252657e7feb8b48b9acc8ba039997528f773
RachelDiamond702 <radi3284@colorado.edu>
2024-11-12 00:32:56 -0700

    wa11

--- js/wa11.js
+++ js/wa11.js
@@ -6,15 +6,43 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const array1 = 
+const images = [`/img/wa11-images/pic1.jpg`, `/img/wa11-images/pic2.jpg`, `/img/wa11-images/pic3.jpg`, `/img/wa11-images/pic4.jpg`, `/img/wa11-images/pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
 
+const alts = {
+  'img/wa11-images/pic1.jpg' : 'Closeup of a human eye',
+  'img/wa11-images/pic2.jpg' : 'Rock that looks like a wave',
+  'img/wa11-images/pic3.jpg' : 'Purple and white pansies',
+  'img/wa11-images/pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'img/wa11-images/pic5.jpg' : 'Large moth on a leaf'
+}
+
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
+  
+  /* Wiring up the Darken/Lighten button */
+  
+  btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
+  
