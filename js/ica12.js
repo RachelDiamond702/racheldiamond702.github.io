134d6c0ee3499d0059b0f698794fe55e99442282
RachelDiamond702 <radi3284@colorado.edu>
2024-11-13 21:40:05 -0700

    ica12

---
+++ js/ica12.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("button");
+const apiEndpoint = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single'
+
+window.addEventListener("load", function() {
+    getJoke();
+});
+
+button.addEventListener("click", function() {
+    getJoke();
+});
+
+function getJoke(){
+    console.log("getJoke function called!");
+    fetch(apiEndpoint)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch the joke');
+        }
+        
+        return response.json();
+
+    })
+    .then(data => {
+
+        if (data.type === 'single') {
+            console.log("Random Joke (as quote): " + data.joke);
+            displayRes(data.joke);
+        } else {
+            console.log("Unexpected joke type received.");
+        }
+    })
+    .catch(error => {
+        console.error("Error fetching joke:", error);
+
+        alert("Error: " + error.message);
+    });
+}
+
+function displayRes(jokeText) {
+    const jokeElement = document.getElementById('joke');
+    jokeElement.innerText = jokeText;
+}
